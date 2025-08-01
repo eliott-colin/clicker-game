5dd2856d6f34b18c8d552b6bd3914ee459c4d563
eliott-colin <eliott.colin.pro@gmail.com>
2025-08-01 14:51:51 +0200

    feat: adding background

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import background from './assets/background.jpg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect , useRef } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [cpm, setCpm] = useState(0);
+   const clickTimestampsRef = useRef([]);
+   
+
+
+
+  // Gère tous les clics (manuels et auto)
+  const registerClick = () => {
+    const now = Date.now();
+
+    clickTimestampsRef.current.push(now);
+
+    clickTimestampsRef.current = clickTimestampsRef.current.filter(
+      (ts) => now - ts <= 1000
+    );
+
+    setCpm(clickTimestampsRef.current.length);
+  };
 
   const clicker = () => {
-    setCount(count + 1);
+    setCount((prev) => prev + 1);
+    registerClick();
   };
+
+  // Mettre à jour le compteur toutes les secondes
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prev) => prev + 1);
+      registerClick();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
 
   return (
     <div className="App" style={{ backgroundImage: `url(${background})`, width  : "100%", height: "100vh", backgroundSize: "cover" }}>
       <div style={{ textAlign: "center"}}>
         <h1 style={{ margin: "0" }}>Clicks: {count}</h1>
+
         <button onClick={clicker}>Clique-moi !</button>
+        <p style={{ marginTop: "20px" }}>Moyenne : {cpm} clicks par secondes.</p>
       </div>
     </div>
   );
